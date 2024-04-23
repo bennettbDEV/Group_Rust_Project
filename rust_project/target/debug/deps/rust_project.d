1e5fc3885a99f8b710b21af476c65f47df670f02
bennettbDEV <bennettbeltran1@gmail.com>
2024-04-22 22:48:46 -0500

    Initial commits

---
+++ rust_project/target/debug/deps/rust_project.d
@@ -0,0 +1,5 @@
+C:\Users\Bennett\Documents\VS_CODE\Group_Rust_Project\rust_project\target\debug\deps\rust_project.exe: src\main.rs
+
+C:\Users\Bennett\Documents\VS_CODE\Group_Rust_Project\rust_project\target\debug\deps\rust_project.d: src\main.rs
+
+src\main.rs:
---
+++ rust_project/target/debug/rust_project.d
@@ -0,0 +1,1 @@
+C:\Users\Bennett\Documents\VS_CODE\Group_Rust_Project\rust_project\target\debug\rust_project.exe: C:\Users\Bennett\Documents\VS_CODE\Group_Rust_Project\rust_project\src\main.rs
