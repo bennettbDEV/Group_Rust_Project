a86be164ad5bea459c4b8e1f26461f292e195afa
naaatami <144642305+naaatami@users.noreply.github.com>
2024-04-28 13:01:02 -0500

    claiming this one

--- intermediate_3/src/main.rs
+++ intermediate_3/src/main.rs
@@ -1,5 +1,8 @@
 // A program that converts binary numbers to base 10 numbers. This will demonstrate loop usage in
 // Rust as well as matching. This will be done through command line arguments.
+// Natalie Simova
+
+use std::env;
 
 fn main() {
     println!("Hello, world!");
