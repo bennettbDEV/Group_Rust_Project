14c5f943d1bbc48f76b708b28a226684393cbdb0
Indigenous1151 <89427482+Indigenous1151@users.noreply.github.com>
2024-04-29 18:46:14 -0500

    Update disjointset.rs
    Added documentation.
    Fixed excessive use of 'as' in find() function.

--- complex_program/src/disjointset.rs
+++ complex_program/src/disjointset.rs
@@ -1,67 +1,68 @@
-// Disjoint Set class implementation including path compression and union by size
-// By Nick Kolesar
+// struct and implementation of a Disjoint Set
+// By: Nick Kolesar
 
 #[derive(Debug)]
+/// struct holding DisjointSet's fields
 pub struct DisjointSet
 {
     the_array: Vec<i32>,
     num_values: u32
 }
 
-
-// "constructor of DisjointSet"
-#[allow(non_snake_case)]
-pub fn DisjointSet(size: u32) -> DisjointSet
+/// Implementation holding all functions of the DisjointSet
+impl DisjointSet 
 {
-    let set = DisjointSet{the_array: vec![-1; size as usize], num_values: size};
-    return set;
-}
-
-
-impl DisjointSet {
-
-    pub fn find(&mut self, object_index: i32) -> i32 
+    /// "Constructor" of DisjointSet
+    #[allow(non_snake_case)]
+    pub fn DisjointSet(size: u32) -> Self
     {
-        if self.the_array[object_index as usize] < 0
-        {
-            return object_index;
-        }
-        else 
-        {
-            self.the_array[object_index as usize] = self.find(self.the_array[object_index as usize]);
-            return self.the_array[object_index as usize];
-        }
+        let set = DisjointSet{the_array: vec![-1; size as usize], num_values: size};
         
+        set
     }
 
-    // Precondition: indicies must be in different sets
-    pub fn union(&mut self, index1: i32, index2: i32) -> bool
-    {
-        let set_label1 = self.find(index1);
-        let set_label2 = self.find(index2);
 
-        if self.the_array[set_label1 as usize] > self.the_array[set_label2 as usize]
+    /// Path Compressing Recursive Find Function
+    pub fn find(&mut self, object_index: i32) -> i32 
+    {
+        let usize_index = object_index as usize;
+
+        if self.the_array[usize_index] < 0
         {
-            self.the_array[set_label2 as usize] += self.the_array[set_label1 as usize];
-            self.the_array[set_label1 as usize] = set_label2;
+            object_index
         }
         else 
         {
-            self.the_array[set_label1 as usize] += self.the_array[set_label2 as usize];
-            self.the_array[set_label2 as usize] = set_label1;  
+            self.the_array[usize_index] = self.find(self.the_array[usize_index]);
+            self.the_array[usize_index]
         }
+    }
 
-        if self.the_array[set_label1 as usize] + self.num_values as i32 == 0 ||
-           self.the_array[set_label2 as usize] + self.num_values as i32 == 0
+    /// Union-by-Size Function 
+    /// Precondition: indicies must be in different sets
+    /// Returns true if all objects are in the same set
+    pub fn union(&mut self, index1: i32, index2: i32) -> bool
+    {
+        let set_label1 = self.find(index1) as usize;
+        let set_label2 = self.find(index2) as usize;
+
+        if self.the_array[set_label1] > self.the_array[set_label2]
+        {
+            self.the_array[set_label2] += self.the_array[set_label1];
+            self.the_array[set_label1] = set_label2 as i32;
+        }
+        else 
+        {
+            self.the_array[set_label1] += self.the_array[set_label2];
+            self.the_array[set_label2] = set_label1 as i32;  
+        }
+        
+        // coerce self.num_values because the_array[...] is type i32
+        if self.the_array[set_label1] + self.num_values as i32 == 0 ||
+           self.the_array[set_label2] + self.num_values as i32 == 0
         { 
             return true; 
         }
         return false; 
-    }
-
-    // helper function for testing functionality of class
-    pub fn get_value(&self, index: usize) -> i32
-    {
-        return self.the_array[index];
     }
 }
