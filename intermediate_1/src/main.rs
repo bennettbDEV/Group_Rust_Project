a7eb4b23f0acd3a36f640825779a72aa087bce53
Bennett <112603507+bennettbDEV@users.noreply.github.com>
2024-05-01 11:38:32 -0500

    Started intermediate1

--- intermediate_1/src/main.rs
+++ intermediate_1/src/main.rs
@@ -1,6 +1,15 @@
 // This program will let the user play hangman through the command line. This will show the usage
 // of arrays in Rust.
 
-fn main() {
-    println!("Hello, world!");
+use random_word::Lang;
+use std::io;
+fn main() 
+{
+    //let word_lengths = 3..8;
+    let word = random_word::gen(Lang::En);
+    let mut guess = String::new();
+    io::stdin()
+        .read_line(&mut guess);
+    println!("Word is: {}",word);
+    println!("You guessed: {guess}");
 }
