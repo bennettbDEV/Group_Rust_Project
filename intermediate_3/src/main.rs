b4adc5c99ea60cb06226a3cce682171a99b0ecb9
naaatami <144642305+naaatami@users.noreply.github.com>
2024-05-07 21:45:14 -0500

    added failure if too few arguments

--- intermediate_3/src/main.rs
+++ intermediate_3/src/main.rs
@@ -1,19 +1,23 @@
-// A program that converts binary numbers to base 10 numbers. This will demonstrate loop usage in Rust as well as matching. This will be done through command line arguments.
-// fix panic if there are no command line arguments and add some comments
+// A program that converts binary numbers to base 10 numbers, demonstrating loop usage and command line arguments.
 
 use std::env;
 
 fn main()
 {
     let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 {
+        print!("Please pass a number to convert to base 10 as a command line argument.\n");
+        return;
+    }
+
     let val_to_convert = &args[1];
     print!("Original binary number: {val_to_convert}\n");
     let binary_number = find_binary(val_to_convert);
     print!("Base 10 number: {binary_number}\n");
-
-
 }
 
+// given a binary number, converts it to a base 10 number
 fn find_binary(val_to_convert: &str) -> i32 {
     let binary_vector: Vec<char> = val_to_convert.chars().collect();
 
